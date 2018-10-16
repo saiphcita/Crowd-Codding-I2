de2bbb3fd4ac0e1fb093626967a44870dee30341
oscar orellana <oscar.orellana.mon@gmail.com>
2018-10-16 12:54:05 -0600

    width listCategort

--- src/Components/Tools/ListCategory.js
+++ src/Components/Tools/ListCategory.js
@@ -1,42 +1,40 @@
-import React from 'react';
-import '../CSS/ListCategory.css';
-import { refGeneralCategory } from './DataBase.js'
+  import React from 'react';
+  import '../CSS/ListCategory.css';
+  import { refGeneralCategory } from './DataBase.js'
 
-class ListCategory extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      category: []
-    };
-  }
+  class ListCategory extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        category: []
+      };
+    }
 
-  componentDidMount() {
-    refGeneralCategory.on("value", (snapshot) => {
-      let category = snapshot.val();
-      this.setState({category : category})
-    });
-  }
+    componentDidMount() {
+      refGeneralCategory.on("value", (snapshot) => {
+        let category = snapshot.val();
+        this.setState({category : category})
+      });
+    }
 
-  render() {
-    return (
-        <div className="DivDefinition2">
-          <div className="titleList2">
-            <li style={{width:"15%", maxWidth:"15%"}}>Category</li>
-            <li style={{width:"47%", maxWidth:"47%"}}>Description</li>
-            <li style={{width:"38%", maxWidth:"38%"}}>Example</li>
+    render() {
+      return (
+          <div className="DivDefinition2">
+            <div className="titleList2">
+              <li style={{width:"18%", maxWidth:"18%"}}>Category</li>
+              <li style={{width:"82%", maxWidth:"82%"}}>Description</li>
+            </div>
+            {this.state.category.map((val, ind) =>{
+              return (
+                <div key={ind} className="Clist">
+                  <li style={{width:"18%", maxWidth:"18%"}}>{val.categoryName}</li>
+                  <li style={{width:"82%", maxWidth:"82%"}}>{val.categoryDefinition}</li>
+                </div>
+              )
+            })}
           </div>
-          {this.state.category.map((val, ind) =>{
-            return (
-              <div key={ind} className="Clist">
-                <li style={{width:"15%", maxWidth:"15%"}}>{val.categoryName}</li>
-                <li style={{width:"47%", maxWidth:"47%"}}>{val.categoryDefinition}</li>
-                <li style={{width:"38%", maxWidth:"38%"}}>{val.categoryExample}</li>
-              </div>
-            )
-          })}
-        </div>
-    );
+      );
+    }
   }
-}
 
-export default ListCategory;
+  export default ListCategory;
